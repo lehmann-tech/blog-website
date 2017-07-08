3a67f5427a0f7127cf3cff82ed3fb846d0ef6e01
Martin Lehmann <martin.lehmann@finn.no>
2017-07-08 22:39:21 +0200

    Turn sidebar expand thingy into a button

--- src/components/sidebar/collapsed-sidebar.js
+++ src/components/sidebar/collapsed-sidebar.js
@@ -3,7 +3,7 @@ import classes from './collapsed-sidebar.css';
 
 const CollapsedSidebar = ({expand}) => (
   <div className={classes.container}>
-    <a href="#" onClick={expand}>≡</a>
+    <button onClick={expand}>≡</button>
   </div>
 );
 
